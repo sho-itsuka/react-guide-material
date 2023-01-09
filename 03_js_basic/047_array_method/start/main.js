a5b7875831837574bfcfbd66a5f31efb8bc976e6
sho-itsuka <sho.itsuka122@gmail.com>
2023-01-09 16:13:23 +0900

    mapメソッド

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,2 +1,13 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
+
+// for文で表現
+for(let i = 0; i < arry.length; i++) {
+  newArry.push(arry[i] * 2)
+}
+
+console.log(newArry);
+
+// mapメソッドを使用
+const newArry2 = arry.map(val => val * 2);
+console.log(newArry2);
