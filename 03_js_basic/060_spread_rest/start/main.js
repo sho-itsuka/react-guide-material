d8bc9bef772a0c42be3ce793241f3f2a97945164
sho-itsuka <sho.itsuka122@gmail.com>
2023-01-09 17:49:03 +0900

    スプレッド演算子

--- 03_js_basic/060_spread_rest/start/main.js
+++ 03_js_basic/060_spread_rest/start/main.js
@@ -1,15 +1,17 @@
 const nums = [3, 1, 4, 1, 5, 10, 2, 6];
-
-const result = Math.max(3, 1, 4, 1, 5, 10, 2, 6);
+// スプレッド演算子で再記述を省略
+const result = Math.max(...nums);
 console.log(result);
 
-
+// 配列で実行
 let arr1 = [1, 2, 3];
 let arr2 = [4, 5, 6];
 let newArr = [...arr1];
+let newArr1 = [...arr1, ...arr2]
+console.log(newArr);
+console.log(newArr1);
 
-console.log(newArr2);
-
+// オブジェクトで実行
 const obj = {
   name: "Tom",
   age: 22,
@@ -18,6 +20,8 @@ const newObj = { ...obj };
 
 console.log(newObj);
 
+
+// 関数で実行
 const restA = (...argA) => console.log(argA);
 restA(1, 3, 4)
 
