cc125a60b889c907e7e0b441405cb47909631cb4
sho-itsuka <sho.itsuka122@gmail.com>
2023-01-09 16:57:53 +0900

    map・filterメソッド

--- 03_js_basic/047_array_method/start/main.js
+++ 03_js_basic/047_array_method/start/main.js
@@ -1,13 +1,37 @@
 const arry = [10, 20, 30, 40];
 const newArry = [];
 
-// for文で表現
+// for(let i = 0; i < arry.length; i++) {
+//     newArry.push(arry[i] * 2)
+// }
+
+// console.log(newArry);
+
+// 上記をmapメソッドで表現
+// const newArry2 = arry.map(val => {
+//   return val * 2;
+// });
+
+// console.log(newArry2);
+
+
+// if文で該当するものを表示
 for(let i = 0; i < arry.length; i++) {
-  newArry.push(arry[i] * 2)
+  const val = arry[i] * 2;
+  if(val > 50) {
+    newArry.push(arry[i] * 2)
+  }
 }
 
 console.log(newArry);
 
-// mapメソッドを使用
-const newArry2 = arry.map(val => val * 2);
-console.log(newArry2);
+// 上記をfilterメソッドで表現
+const newArry2 = arry.map(val => {
+  return val * 2;
+});
+const newArray3 = newArry2.filter(val => val > 50);
+console.log(newArray2);
+
+//1文にまとめる
+// const newArry2 = arry.map(val => val * 2).filter(val => val > 50);
+// console.log(newArray2);
