86e0c426114d98e6d61d7a6584399ee0944f88a3
sho-itsuka <sho.itsuka122@gmail.com>
2023-01-09 20:48:55 +0900

    await/async

--- 03_js_basic/100_async_await/start/main.js
+++ 03_js_basic/100_async_await/start/main.js
@@ -1,17 +1,34 @@
-// 非同期処理（Promise）
+// 非同期処理（await/async）
+// thenメソッドを使用せずに書き換える
 let a = 0;
 
-new Promise((resolve, reject) => {
+init();
+async function init() {
+  // resolve(a)が呼ばれるまで待機→resultに格納される
+  const result = await new Promise((resolve, reject) => {
     setTimeout(() => {
-        a = 1;
-        resolve(a)
+      a = 1;
+      resolve(a)
     }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
-})
+  })
+  console.log(result);
+  // }).catch((c) => {
+  //     console.log('catchが実行', c)
+  // })
+}
 
+// 例外処理
+init();
+async function init() {
+  try {
+    const result = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        a = 1;
+        reject(a)
+      }, 2000);
+    })
+    console.log(result);
+  } catch(e) {
+    console.log('catchが実行', e);
+  }
+}
