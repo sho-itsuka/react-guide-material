ef0040fe0c470477c4bfce1d6a6738906c7e3745
sho-itsuka <sho.itsuka122@gmail.com>
2023-01-09 15:50:19 +0900

    DOMとイベントリスナー

--- 03_js_basic/045_eventlistener/start/main.js
+++ 03_js_basic/045_eventlistener/start/main.js
@@ -4,7 +4,14 @@ console.log(h1Element.textContent);
 h1Element.textContent = '変更後のタイトル';
 
 // ボタン押下後にダイアログ出力
-const btn = document.querySelector('button');
-btn.addEventListener('click', (e) => {
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', (e) => {
+//   alert('hello');
+// });
+
+// addEventListenerの第二引数に関数を渡す場合
+const btnFn = e => {
   alert('hello');
-});
+}
+const btn = document.querySelector('button');
+btn.addEventListener('click', btnFn);
