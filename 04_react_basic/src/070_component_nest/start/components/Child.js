2319b784763676cdfb2a833fda89788395dee9a9
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-14 20:15:57 +0900

    Update Child.js

--- 04_react_basic/src/070_component_nest/start/components/Child.js
+++ 04_react_basic/src/070_component_nest/start/components/Child.js
@@ -9,3 +9,20 @@ const Child = () {
     </div>
   );
 };
+
+// POINT コンポーネントを default export
+export default Child;
+
+
+/* POINT default export の別の記法
+
+export default () => {
+  return (
+    <div className="component">
+      <h3>Hello Component</h3>
+      <List />
+    </div>
+  );
+};
+
+ */
