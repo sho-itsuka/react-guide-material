9c3d5f04b5a02246b6dee84e3fbe65ddf42c4f25
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-20 01:56:48 +0900

    Update Example.js

--- 04_react_basic/src/120_props_rules/start/Example.js
+++ 04_react_basic/src/120_props_rules/start/Example.js
@@ -1,14 +1,15 @@
 // POINT propsの流れは一方通行
 // POINT propsは読み取り専用
 
-import Bye from "./components/Bye"
+import Bye   from "./components/Bye"
 import Hello from "./components/Hello"
 
 const Example = () => {
+  const myName = 'Tom';
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={myName} />
+      <Bye name={myName} />
     </>
   );
 };
