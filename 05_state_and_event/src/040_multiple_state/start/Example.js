7dcee9c6631e7e3e70ca4450121361c5969e341b
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 18:53:34 +0900

    study about state(count button)

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,27 @@
+import { useState } from 'react';
+
 const Example = () => {
-  
+  const [ countA, setCountA ] = useState(0);
+  const [ countB, setCountB ] = useState(10);
+  const [ countC, setCountC ] = useState(100);
+  return (
+    <>
+      <p>ボタンAを{countA}回押しました！</p>
+      <button onClick={() => {
+        setCountA(countA + 1);
+      }}>ボタンA</button>
+
+      <p>ボタンBを{countB}回押しました！</p>
+      <button onClick={() => {
+        setCountB(countB + 2);
+      }}>ボタンB</button>
+
+      <p>ボタンC{countC}回押しました！</p>
+      <button onClick={() => {
+        setCountC(countC + 3);
+      }}>ボタンC</button>
+    </>
+  )
 };
 
 export default Example;
