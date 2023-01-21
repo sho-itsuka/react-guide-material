56e7230fa9208db346696f91c22cd4401b231bfd
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 19:22:05 +0900

    study about state(count up)

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,19 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+  const [ count, setCount ] = useState(0);
+
+  return (
+    <>
+      <p>現在のカウント数: {count}</p>
+      <button onClick={() => {
+        setCount(count + 2);
+      }}>+</button>
+      <button onClick={() => {
+        setCount(count - 1);
+      }}>-</button>
+    </>
+  );
 };
 
 export default Example;
