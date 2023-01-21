319ccdcca1689ba39171b17a4a9c385c9b22d7bd
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 17:53:59 +0900

    分割代入で書き換え

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,5 +1,21 @@
 import { useState } from 'react';
 
+// const Example = () => {
+//   let valArray = useState();
+//   return (
+//     <>
+//       <input
+//       type="text"
+//       onChange={(e) => {
+//         const setFn = valArray[1];
+//         setFn(e.target.value)
+//         }} /> = {valArray[0]}
+//     </>
+//   );
+// };
+
+
+// 分割代入を用いて書き換え(基本的にはこちらの記述方法を使用！)
 const Example = () => {
   const [val, setVal] = useState();
 
