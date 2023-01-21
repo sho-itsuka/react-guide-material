d8168311fd1f919b96a7afa00592609ee5ca7754
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 17:52:45 +0900

    study about useState

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+
 const Example = () => {
+  const [val, setVal] = useState();
 
   return (
     <>
-      
+      <input
+      type="text"
+      onChange={(e) => {
+        setVal(e.target.value)
+      }} = {val}
     </>
   );
 };
