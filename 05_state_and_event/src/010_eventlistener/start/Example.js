543b87dfab7c37fb1e729a9b87b25074ac0cacaf
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 14:44:26 +0900

    イベントリスナー

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,16 @@
 const Example = () => {
+  const clickHundler = () => {
+    alert('ボタンがクリックされました')
+  };
+  const clickHundler2 = () => {
+    alert('これもボタンです')
+  };
+  
   return (
-    <></>
+    <>
+      <button onClick={clickHundler}>クリックしてね</button>
+      <button onClick={clickHundler2}>クリックしてね</button>
+    </>
   );
 };
 
