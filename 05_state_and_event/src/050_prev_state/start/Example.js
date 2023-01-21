fc09c23ed87b55c13e97aa6beba33f66af9f27c3
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-21 19:35:32 +0900

    add num function

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,29 +1,15 @@
 import { useState } from "react";
 
-// const Example = () => {
-//   const [ count, setCount ] = useState(0);
-// 
-//   return (
-//     <>
-//       <p>現在のカウント数: {count}</p>
-//       <button onClick={() => {
-//         setCount(count + 2);
-//       }}>+</button>
-//       <button onClick={() => {
-//         setCount(count - 1);
-//       }}>-</button>
-//     </>
-//   );
-// };
-
-// 関数を用いて書き換え
-
 const Example = () => {
   const [ count, setCount ] = useState(0);
-  
-  const countUp = () => {
-    setCount(count + 2);
-  }
+
+  const countUp   = () => {
+    setCount(count + 1);
+    setCount(num => {
+      return num + 1 
+    });
+  };
+
   const countDown = () => {
     setCount(count - 1);
   }
