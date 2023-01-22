857baf8622e2e6c651783e005c271feed9fdeaa5
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-22 15:15:47 +0900

    study about state(object)

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -1,7 +1,34 @@
+// プリミティブ型: 1, "str", bool, 10n, Symbol(), null, undefined
+// オブジェクト型: {}, [] などのプリミティブ型以外
+
 import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj);
+  
+  const changeName = (e) => {
+    setPerson({name: e.target.value, age: person.age})
+  }
+
+  const changeAge = (e) => {
+    setPerson({name: person.name, age: e.target.value})
+  }
+  
+  return (
+    <>
+      <h3>Name: {person.name}</h3>
+      <h3>Age:  {person.age}</h3>
+      <input
+        type="text"
+        onChange={changeName}
+      />
+      <input
+        type="text"
+        onChange={changeAge}
+      />
+    </>
+  )
 };
 
 export default Example;
