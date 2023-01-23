194461593f929a17014d1a7e076e22130afe00d5
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-24 00:28:01 +0900

    filterメソッドで結果が一致するものを抽出して表示

--- 06_control_and_form/src/040_list_and_filter/start/Example.js
+++ 06_control_and_form/src/040_list_and_filter/start/Example.js
@@ -3,13 +3,25 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [ filterVal, setFilterVal ] = useState("");
+
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => {
+          setFilterVal(e.target.value)
+        }}
+      />
       <ul>
         {animals
+        // filter():  配列の中から、提供された関数で実装されたテストに合格した要素(true)のみを抽出
+        // indexOf(): 呼び出す String オブジェクト中で、 fromIndex から検索を始め、指定された値が最初に現れたインデックスを返す。値が見つからない場合は -1 を返す。
+        .filter(animal => animal.indexOf(filterVal) !== -1)
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
