627ce33db48c86916b8504f841db0de687b39912
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-25 00:33:14 +0900

    問題の解答

--- 06_control_and_form/src/050_practice_filter/start/Example.js
+++ 06_control_and_form/src/050_practice_filter/start/Example.js
@@ -1,32 +1,45 @@
-import Profile from "./components/Profile";
+import Profile      from "./components/Profile";
+import { useState } from "react";
 
 const persons = [
   {
-    name: "Geo",
-    age: 18,
+    name:    "Geo",
+    age:     18,
     hobbies: ["sports", "music"],
   },
   {
-    name: "Tom",
-    age: 25,
+    name:    "Tom",
+    age:     25,
     hobbies: ["movie", "music"],
   },
   {
-    name: "Lisa",
-    age: 21,
+    name:    "Lisa",
+    age:     21,
     hobbies: ["sports", "travel", "game"],
   },
 ];
 
 const Example = () => {
+  const [filterVal, setFilterVal] = useState("");
+
   return (
     <>
+      <input
+        type="text"
+        value={filterVal}
+        onChange={(e) => setFilterVal(e.target.value)}
+      />
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>
-            <Profile {...person} />
-          </li>
-        ))}
+        {persons
+          .filter(person => {
+            const isMatch = person.name.indexOf(filterVal) !== -1
+            return isMatch;
+          })
+          .map((person) => (
+            <li key={person.name}>
+              <Profile {...person} />
+            </li>
+          ))}
       </ul>
     </>
   );
