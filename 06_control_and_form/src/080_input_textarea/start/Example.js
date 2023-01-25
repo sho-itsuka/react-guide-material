df27a38ac55126aa481758b9e1296f362e215d30
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-26 00:07:31 +0900

    input要素とtextarea要素の使い方

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -3,22 +3,19 @@ import { useState } from "react";
 const Example = () => {
   const [val, setVal] = useState("");
   const clearVal = () => setVal("");
+
   return (
     <div>
       <label htmlFor="456">ラベル</label>
       <div>
         <input
           id="123"
-          type="text"
           placeholder="こんにちは"
           value={val}
           onChange={(e) => setVal(e.target.value)}
           />
-      </div>
-      <div>
-        <input
+        <textarea
           id="456"
-          type="text"
           placeholder="こんにちは"
           value={val}
           onChange={(e) => setVal(e.target.value)}
