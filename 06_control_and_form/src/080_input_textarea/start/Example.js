0d95ef424dfcde4053931c917723b06ff56d597f
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-26 00:15:20 +0900

    リファクタリング

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -2,24 +2,26 @@ import { useState } from "react";
 
 const Example = () => {
   const [val, setVal] = useState("");
-  const clearVal = () => setVal("");
+  const clearVal      = () => setVal("");
 
   return (
     <div>
-      <label htmlFor="456">ラベル</label>
+      {/* label要素の値を押下すると、htmlForと同じ値を持つidがフォーカスされる */}
+      <label htmlFor="456">ラベル</label> 
       <div>
         <input
           id="123"
           placeholder="こんにちは"
           value={val}
           onChange={(e) => setVal(e.target.value)}
-          />
+        />
+        {/* JXSの場合は閉じタグ不要(inputと同じように記述する) */}
         <textarea
           id="456"
           placeholder="こんにちは"
           value={val}
           onChange={(e) => setVal(e.target.value)}
-          />
+        />
       </div>
       <h3>{val}</h3>
       <button onClick={clearVal}>クリア</button>
