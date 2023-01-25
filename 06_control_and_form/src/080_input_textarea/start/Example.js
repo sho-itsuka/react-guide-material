50580fd7001e1534898f341948659d1943bb16ca
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-25 23:56:05 +0900

    Update Example.js

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [val, setVal] = useState("");
   return (
     <div>
       <label htmlFor="456">ラベル</label>
       <div>
         <input
+          id="123"
           type="text"
           placeholder="こんにちは"
-          
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
           />
       </div>
     </div>
