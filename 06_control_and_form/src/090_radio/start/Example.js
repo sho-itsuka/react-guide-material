f9607b5e5374b5e9ff6fce1e765db6c112024912
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-27 23:02:06 +0900

    Update Example.js

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 const Example = () => {
+  const [fruit, setFruit] = useState("Apple");
+  const onChange = (e) => setFruit(e.target.value);
+  
+  const RADIO_COLLECTION = ["Apple", "banana", "cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map((value) => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={val}
+              checked={val === value}
+            />
+            {value}
+          </label>
   );
 };
 
