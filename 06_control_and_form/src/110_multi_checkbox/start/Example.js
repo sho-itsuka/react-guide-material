6bb78d884783b77112355f9f69e3fdb17b39e8c8
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-28 16:55:40 +0900

    handleChangeの実装

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -14,7 +14,55 @@ const Example = () => {
   return (
     <div>
       {fruits.map(fruit => {
+        return (import { useState } from "react";
+
+const Example = () => {
+  const [fruits, setFruits] = useState([
+    { label: "Apple",  value: 100, checked: false },
+    { label: "Banana", value: 200, checked: false },
+    { label: "Cherry", value: 300, checked: false },
+  ]);
+
+  const [sum, setSum] = useState(0);
+
+  const handleChange = (e) => {
+    // newFruits という変数に再代入
+    const newFruits = fruits.map(fruit => {
+      // 新しい変数に、スプレッド演算子で再代入する(fruitオブジェクトには1つずつオブジェクトが渡ってくる)
+      const newFruit = { ...fruit };
+      if(newFruit.label === e.target.value) {
+        // 現在はchecked: false なので、! をつけることでtrue を表現
+        newFruit.checked = !fruit.checked;
+      }
+      return newFruit;
+    })
+
+    setFruits(newFruits);
+  };
+
+  return (
+    <div>
+      {fruits.map((fruit) => {
         return (
+          <div key={fruit.label}>
+            <input
+              id={fruit.label}
+              type="checkbox"
+              value={fruit.label}
+              checked={fruit.checked}
+              onChange={handleChange}
+            />
+            <label htmlFor={fruit.label}>{fruit.label}:{fruit.value}</label>
+          </div>
+        )
+      })}
+      <div>合計：{sum}</div>
+    </div>
+  );
+};
+
+export default Example;
+
           <div key={fruit.label}>
             <input
               id={fruit.label}
