d4b64eab6421e43e76bb5d02e6b563637e8e1fb2
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-28 14:37:29 +0900

    map()での書き換え

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,41 +1,68 @@
 import { useState } from "react";
 
+// const Example = () => {
+//   const [fruit, setFruit] = useState("Apple");
+//   const onChange = (e) => setFruit(e.target.value);
+
+//   return (
+//     <>
+//       <label>
+//         <input
+//           type="radio"
+//           value={"Apple"}
+//           checked={fruit === "Apple"}
+//           onChange={onChange}
+//         />
+//         Apple
+//       </label>
+//       <label>
+//         <input
+//           type="radio"
+//           value={"Banana"}
+//           checked={fruit === "Banana"}
+//           onChange={onChange}
+//         />
+//         Banana
+//       </label>
+//       <label>
+//         <input
+//           type="radio"
+//           value={"Cherry"}
+//           checked={fruit === "Cherry"}
+//           onChange={onChange}
+//         />
+//         Cherry
+//       </label>
+//       <h3>私は{fruit}が食べたい</h3>
+//     </>
+//   );
+// };
+
+// map() メソッドを用いて上記コードを書き換え
 const Example = () => {
-  const [fruit, setFruit] = useState("Apple");
+  const [fruit, setFruit] = useState("");
   const onChange = (e) => setFruit(e.target.value);
+
+  const radioCollection = ["Apple", "Banana", "Cherry"]
 
   return (
     <>
-      <label>
-        <input
-          type="radio"
-          value={"Apple"}
-          checked={fruit === "Apple"}
-          onChange={onChange}
-        />
-        Apple
-      </label>
-      <label>
-        <input
-          type="radio"
-          value={"Banana"}
-          checked={fruit === "Banana"}
-          onChange={onChange}
-        />
-        Banana
-      </label>
-      <label>
-        <input
-          type="radio"
-          value={"Cherry"}
-          checked={fruit === "Cherry"}
-          onChange={onChange}
-        />
-        Cherry
-      </label>
+      {radioCollection.map((value) => {
+        return (
+          <label>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+            {value}
+          </label>
+        )
+      })}
       <h3>私は{fruit}が食べたい</h3>
     </>
-  );
-};
+  )
+}
 
 export default Example;
