eddc27a35178f6b0e26ecb09ded0b54c8d0e45a7
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-28 16:40:54 +0900

    複数のチェックボックスを実装

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -2,16 +2,31 @@ import { useState } from "react";
 
 const Example = () => {
   const [fruits, setFruits] = useState([
-    { label: "Apple", value: 100, checked: false },
+    { label: "Apple",  value: 100, checked: false },
     { label: "Banana", value: 200, checked: false },
     { label: "Cherry", value: 300, checked: false },
   ]);
 
   const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {};
+
   return (
     <div>
-      
+      {fruits.map(fruit => {
+        return (
+          <div key={fruit.label}>
+            <input
+              id={fruit.label}
+              type="checkbox"
+              value={fruit.label}
+              checked={fruit.checked}
+              onChange={handleChange}
+            />
+            <label htmlFor={fruit.label}>{fruit.label}:{fruit.value}</label>
+          </div>
+        )
+      })}
       <div>合計：{sum}</div>
     </div>
   );
