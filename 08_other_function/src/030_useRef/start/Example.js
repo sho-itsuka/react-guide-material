7e289ad504002750ccd51d872aceaede7d4766cd
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-01-30 22:56:19 +0900

    study useRef

--- 08_other_function/src/030_useRef/start/Example.js
+++ 08_other_function/src/030_useRef/start/Example.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import { useRef }   from "react";
 
 const Case1 = () => {
   const [value, setValue] = useState("");
-
+  const inputRef = useRef()
+  // refオブジェクトは、currentプロパティを必ず持っている
+  // 上記に対して、ref属性で渡した際に current プロパティに対して値を保持できる
   return (
     <div>
       <h3>ユースケース1</h3>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button onClick>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
         インプット要素をフォーカスする
       </button>
     </div>
