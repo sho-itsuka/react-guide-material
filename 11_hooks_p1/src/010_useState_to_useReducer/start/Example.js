6a386bd2a8c0d3da153615f3a1ee2b41e502e3b0
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-02-11 21:01:10 +0900

    Update Example.js

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,50 @@
+import {useReducer, useState} from "react";
+
 const Example = () => {
+  const [state, setState]  = useState(0);
+  const [rstate, dispatch] = useReducer((prev, {type, step}) => {
+    switch(type) {
+      case '+':
+        return prev + step;
+      case '-':
+        return prev - step;
+      default:
+        throw new Error('不明なアクションです。');
+    }
+    // if (action === '+') {
+    //   return prev + 2;
+    // } else if (action === '-') {
+    //   return prev - 3;
+    // }
+  }, 0);
+
+  const countUp    = () => {
+    setState(prev => ++prev);
+  }
+
+  const rcountUp   = () => {
+    dispatch({ type: '+', step: 2 });
+  }
+
+  const rcountDown = () => {
+    dispatch({ type: '-', step: 3} );
+  }
   
   return (
     <>
+      <div>
+        <h3>State</h3>
+        <p>{state}</p>
+        <button
+          onClick={countUp}
+        >+</button>
+      </div>
+      <div>
+        <h3>Reducer</h3>
+        <p>{rstate}</p>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
     </>
   );
 };
