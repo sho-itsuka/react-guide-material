debf25e60a01c531284b83ad56e7677a1045b6df
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-02-18 20:49:49 +0900

    Update Example.js

--- 11_hooks_p1/src/060_context_file_render/start/Example.js
+++ 11_hooks_p1/src/060_context_file_render/start/Example.js
@@ -1,13 +1,17 @@
-import "./Example.css";
-import Main from "./components/Main";
-import Header from "./components/Header";
+import Main              from "./components/Main";
+import Header            from "./components/Header";
+import Footer            from "./components/Footer";
+
 import { ThemeProvider } from "./context/ThemeContext";
+
+import "./Example.css";
 
 const Example = () => {
   return (
     <ThemeProvider>
       <Header />
       <Main />
+      <Footer />
     </ThemeProvider>
   );
 };
