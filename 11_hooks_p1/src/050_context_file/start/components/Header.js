1788ccca0f78efdf83ea512333c314d2cfc8b3d5
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-02-18 18:26:07 +0900

    リファクタリング(createContext を更に切り分け)

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,8 +1,7 @@
-import { useContext }   from "react";
-import { ThemeContext } from "../Example";
+import { useTheme } from "../context/ThemeContext";
 
 const Header = () => {
-  const [theme, setTheme] = useContext(ThemeContext);
+  const [theme, setTheme] = useTheme();
 
   const THEMES = ['light', 'dark', 'red'];
 
@@ -13,10 +12,9 @@ const Header = () => {
       <header className={`content-${theme}`}>
         {THEMES.map(_theme => {
           return (
-            <label>
+            <label key={_theme}>
               <input
                 type="radio"
-                key={_theme}
                 value={_theme}
                 checked={theme === _theme}
                 onChange={changeTheme}
