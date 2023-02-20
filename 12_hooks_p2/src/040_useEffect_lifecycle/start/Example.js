bd77c52cd09123dd1d0b74e7c650eae754c66efc
@shota <101057902+sho-itsuka@users.noreply.github.com>
2023-02-21 00:01:38 +0900

    Update Example.js

--- 12_hooks_p2/src/040_useEffect_lifecycle/start/Example.js
+++ 12_hooks_p2/src/040_useEffect_lifecycle/start/Example.js
@@ -1,6 +1,36 @@
+// POINT useEffectの実行タイミング
+
+import { useState, useEffect } from "react";
+
 const Example = () => {
+  const [state, setState] = useState(0);
+
+  useEffect(
+    function update() {
+      console.log("update");
+
+      return function cleanUp() {
+        console.log("update cleanup");
+      };
+    },
+    [state]
+  );
+
+  useEffect(() => {
+    console.log("mount");
+
+    return () => {
+      console.log("mount cleanup");
+    };
+  }, []);
+
+  console.log("render");
+  
   return (
-    <h3>useEffectの呼ばれるタイミングをコンソールで確認してみよう</h3>
+    <>
+      <button onClick={() => setState((prev) => prev + 1)}>更新</button>
+      <h3>他のレクチャーを選ぶとunmountが呼ばれます。</h3>
+    </>
   );
 };
 
